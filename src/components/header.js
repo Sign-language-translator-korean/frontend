184d22470246429ef781614f1f09278da9e51006
pepper-sister <yuyu7123@naver.com>
2025-03-20 12:11:34 +0900

    feat: 페이지 이동 기능 추가

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import '../styles/header.css'
 
 const Header = () => {
@@ -5,7 +6,9 @@ const Header = () => {
   return (
     <header>
       <div className="header">
+        <Link to="/">
         <img className="header__logo" src="/logo.svg" alt="Designed by Freepik" />
+        </Link>
         <div className="header__name">수어 번역기</div>
       </div>
     </header>
